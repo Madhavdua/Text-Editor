6ebefdd861f86583c5bb1445b924eb7d71e3dd4b
Madhav Dua <madhavdua26@gmail.com>
2023-02-14 21:50:09 +0530

    Updates

---
+++ precache-manifest.08f24a28957988ddd9df53f65ab4cff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "510b435a5e97fd9b1e5d",
+    "url": "/Madhavdua/firstReactApp/static/css/main.2c492ab6.chunk.css"
+  },
+  {
+    "revision": "510b435a5e97fd9b1e5d",
+    "url": "/Madhavdua/firstReactApp/static/js/main.510b435a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Madhavdua/firstReactApp/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Madhavdua/firstReactApp/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "6d44f83d2dc3797a6000",
+    "url": "/Madhavdua/firstReactApp/static/js/runtime~main.6d44f83d.js"
+  },
+  {
+    "revision": "6abbdab480feb100569663b929bfb1ab",
+    "url": "/Madhavdua/firstReactApp/index.html"
+  }
+];
